795349d11d54ddda832ae621ef120ded6a5064bc
guilherme-marcello <guilemosm@gmail.com>
2022-09-23 23:56:18 +0100

    Implemented Record&Play plugin handler

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"strconv"
 
+	"github.com/guilherme-marcello/janus/elements"
 	"github.com/guilherme-marcello/janus/requests"
 	"github.com/guilherme-marcello/janus/session"
 )
@@ -17,8 +18,8 @@ type Plugin struct {
 	endpoint string
 }
 
-func New(session session.Session, name string) Plugin {
-	plugin := Plugin {
+func newPluginHandler(session session.Session, name string) Plugin {
+	plugin := Plugin{
 		session: session,
 		name:    name,
 	}
@@ -47,4 +48,24 @@ func (_plugin Plugin) String() string {
 		_plugin.id,
 		_plugin.endpoint,
 	)
+}
+
+type RecordPlay struct {
+	Plugin
+}
+
+func NewRecordPlayHandler(session session.Session) RecordPlay {
+	return RecordPlay{
+		newPluginHandler(session, "janus.plugin.recordplay"),
+	}
+}
+
+func (_recordplay RecordPlay) List() []elements.Recording {
+	response := requests.GetPostResponse(_recordplay.endpoint, requests.LIST_RECORDPLAY())
+	model := &requests.MODEL_LIST_RECORDPLAY{}
+	err := json.NewDecoder(response.Body).Decode(&model)
+	if err != nil || model.Janus == "error" {
+		log.Printf("Failed list recordings with plugin %s at %s", _recordplay.name, _recordplay.session.Endpoint)
+	}
+	return model.Plugindata.Data.List
 }
