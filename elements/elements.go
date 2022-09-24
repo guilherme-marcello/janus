809a2a4901b61a7e3bf57872c0b1940c947b2052
guilherme-marcello <guilemosm@gmail.com>
2022-09-24 20:37:30 +0100

    Added Mountpoint element

--- elements/elements.go
+++ elements/elements.go
@@ -10,3 +10,17 @@ type Recording struct {
 	VideoCodec string `json:"video_codec,omitempty"`
 	Data       bool   `json:"data"`
 }
+
+type Mountpoint struct {
+	ID          int    `json:"id"`
+	Type        string `json:"type"`
+	Description string `json:"description"`
+	Metadata    string `json:"metadata"`
+	Enabled     bool   `json:"enabled"`
+	Media       []struct {
+		Mid   string `json:"mid"`
+		Type  string `json:"type"`
+		Label string `json:"label"`
+		AgeMs int    `json:"age_ms"`
+	} `json:"media,omitempty"`
+}
