4f656e9c97fb95d438967c64aaf0545cae0e2543
guilherme-marcello <guilemosm@gmail.com>
2022-09-24 20:44:36 +0100

    Implemented Streaming plugin handler

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -70,3 +70,23 @@ func (_recordplay RecordPlay) List() []elements.Recording {
 	}
 	return model.Plugindata.Data.List
 }
+
+type Streaming struct {
+	Plugin
+}
+
+func NewStreamingHandler(session session.Session) Streaming {
+	return Streaming{
+		newPluginHandler(session, "janus.plugin.streaming"),
+	}
+}
+
+func (_streaming Streaming) List() []elements.Mountpoint {
+	response := requests.GetPostResponse(_streaming.endpoint, requests.LIST_REQUEST())
+	model := &requests.MODEL_LIST_STREAMING{}
+	err := json.NewDecoder(response.Body).Decode(&model)
+	if err != nil || model.Janus == "error" {
+		log.Printf("Failed to list mountpoints with plugin %s at %s", _streaming.name, _streaming.session.Endpoint)
+	}
+	return model.Plugindata.Data.List
+}
