bdf55aa683fda340771a59b1b2443813bd375379
guilherme-marcello <guilemosm@gmail.com>
2023-03-26 13:39:28 +0100

    Added error handling to Record&Play

--- plugin/recordnplay.go
+++ plugin/recordnplay.go
@@ -13,18 +13,23 @@ type RecordPlay struct {
 	Plugin
 }
 
-func NewRecordPlayHandler(session session.Session) RecordPlay {
-	return RecordPlay{
-		newPluginHandler(session, "janus.plugin.recordplay"),
+func NewRecordPlayHandler(session session.Session) (RecordPlay, error) {
+	plugin, err := newPluginHandler(session, "janus.plugin.recordplay")
+	if err != nil {
+		return RecordPlay{}, err
 	}
+	return RecordPlay{plugin}, nil
 }
 
-func (_recordplay RecordPlay) List() []elements.Recording {
-	response := requests.GetPostResponse(_recordplay.endpoint, requests.LIST_REQUEST())
+func (_recordplay RecordPlay) List() ([]elements.Recording, error) {
+	response, err := requests.SendHTTPRequest("POST", _recordplay.endpoint, requests.LIST_REQUEST())
+	if err != nil {
+		return nil, err
+	}
 	model := &requests.MODEL_LIST_RECORDPLAY{}
-	err := json.NewDecoder(response.Body).Decode(&model)
+	err = json.NewDecoder(response.Body).Decode(&model)
 	if err != nil || model.Janus == "error" {
 		log.Printf("Failed list recordings with plugin %s at %s", _recordplay.name, _recordplay.session.Endpoint)
 	}
-	return model.Plugindata.Data.List
+	return model.Plugindata.Data.List, nil
 }
