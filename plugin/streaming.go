30174a68ea055ab91aba923d33cf302baf7236ee
guilherme-marcello <guilemosm@gmail.com>
2023-03-26 13:41:09 +0100

    Added error handling to Streaming

--- plugin/streaming.go
+++ plugin/streaming.go
@@ -13,18 +13,23 @@ type Streaming struct {
 	Plugin
 }
 
-func NewStreamingHandler(session session.Session) Streaming {
-	return Streaming{
-		newPluginHandler(session, "janus.plugin.streaming"),
+func NewStreamingHandler(session session.Session) (Streaming, error) {
+	plugin, err := newPluginHandler(session, "janus.plugin.streaming")
+	if err != nil {
+		return Streaming{}, err
 	}
+	return Streaming{plugin}, nil
 }
 
-func (_streaming Streaming) List() []elements.Mountpoint {
-	response := requests.GetPostResponse(_streaming.endpoint, requests.LIST_REQUEST())
+func (_streaming Streaming) List() ([]elements.Mountpoint, error) {
+	response, err := requests.SendHTTPRequest("POST", _streaming.endpoint, requests.LIST_REQUEST())
+	if err != nil {
+		return nil, err
+	}
 	model := &requests.MODEL_LIST_STREAMING{}
-	err := json.NewDecoder(response.Body).Decode(&model)
+	err = json.NewDecoder(response.Body).Decode(&model)
 	if err != nil || model.Janus == "error" {
 		log.Printf("Failed to list mountpoints with plugin %s at %s", _streaming.name, _streaming.session.Endpoint)
 	}
-	return model.Plugindata.Data.List
+	return model.Plugindata.Data.List, nil
 }
